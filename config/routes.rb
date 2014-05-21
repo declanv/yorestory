673669f333d87359c01714e7bbb434f1c56540dd
Crustacean87! <declanv@gmail.com>
2014-05-21 17:46:31 -0400

    Removed basic_auth routes for a couple of controllers

--- config/routes.rb
+++ config/routes.rb
@@ -46,11 +46,11 @@ get "/milestones/:id/edit" => "milestones#edit", as: :edit_milestone
 patch "/milestones/:id" => "milestones#update"
 
 # # pasted from: http://railscasts.com/episodes/250-authentication-from-scratch
-get "log_out" => "sessions#destroy", :as => "log_out"
-get "log_in" => "sessions#new", :as => "log_in"
-get "sign_up" => "users#new", :as => "sign_up"
-# root :to => "users#new"
-resources :users
-resources :sessions
+# get "log_out" => "sessions#destroy", :as => "log_out"
+# get "log_in" => "sessions#new", :as => "log_in"
+# get "sign_up" => "users#new", :as => "sign_up"
+# # root :to => "users#new"
+# resources :users
+# resources :sessions
 
 end
