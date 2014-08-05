2d00417acd8f821c05922be67ea645f81459e428
Crustacean87! <declanv@gmail.com>
2014-08-05 03:31:32 -0400

    Added a users index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+    render(:index)
+  end
+
   def show
     # show me one user
      @user = User.find(params[:id])
